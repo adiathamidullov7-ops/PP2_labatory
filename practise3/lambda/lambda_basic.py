03b796b9903b7ebf74faf939e065ed2446d89c6e
adiat <adiathamidullov7@gmail.com>
2026-02-20 01:12:46 +0500

    lambda

---
+++ practise3/lambda/lambda_basic.py
@@ -0,0 +1,18 @@
+x = lambda a:a+10
+print(x(5))
+
+func = lambda x, y: x+y
+res = func(5, 4)
+print(res)
+
+x = lambda a, b: a*b
+les = x(4,6)
+print(les)
+
+y = lambda n, m: n-m
+les = y(5, 8)
+print(les)
+
+h = lambda g,v: (g+v)-v
+fes = h(20, 5)
+print(fes)
