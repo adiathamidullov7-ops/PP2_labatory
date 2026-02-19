a9678d57113fcd0ecbf1bcedadbb79f3d5df7108
adiat <adiathamidullov7@gmail.com>
2026-02-19 23:18:11 +0500

    Basic Functions

---
+++ practise3/function/basic_functions.py
@@ -0,0 +1,21 @@
+def my_functions():
+    print("Hello Everyone")  #Creating Functions
+
+
+    my_funcions() #Calling a Function 
+
+def fahrenheit_to_celsius(fahranheit):
+    return(fahranheit - 32) *5 /9
+print(fahrenheit_to_celsius(32))
+print(fahrenheit_to_celsius(52))
+print(fahrenheit_to_celsius(67))  #all  the functions
+
+
+def get_generating():
+    return "Hello Everyone"
+message = get_generating()
+print(message) #Return Values
+
+def my_functions():
+    pass #Pass the statements
+
