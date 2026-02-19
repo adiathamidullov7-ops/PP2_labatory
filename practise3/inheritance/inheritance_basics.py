26165fe8d24f0632231a82d314787d46d1e4d08e
adiat <adiathamidullov7@gmail.com>
2026-02-20 01:37:52 +0500

    basic

--- practise3/inheritance/inheritance_basics.py
+++ practise3/inheritance/inheritance_basics.py
@@ -1,19 +1,59 @@
-x = lambda a:a+10
-print(x(5))
+class Animal:
+    def speak(self):
+        return "Sound"
+class Cat(Animal):
+    def speak(self):
+        return "MEOW"
+print(Cat.speak())
 
-func = lambda x, y: x+y
-res = func(5, 4)
-print(res)
 
-x = lambda a, b: a*b
-les = x(4,6)
-print(les)
 
-y = lambda n, m: n-m
-les = y(5, 8)
-print(les)
 
-h = lambda g,v: (g+v)-v
-fes = h(20, 5)
-print(fes)
+
+class Grandparent:
+    def house(ielf):
+        return "Big House"
+
+class Parent(Grandparent):
+    def car(ielf):
+        return "a Car"
+class Child(Parent):
+    def phone(ielf):
+        return "Phone"
+child = Child()
+print(child.house())
+print(child.car)
+print(child.phone())
+
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    def info(self):
+        return f"Имя: {self.name}"
+
+class Student(Person):
+    def __init__(self, name, grade):
+        super().__init__(name)
+        self.grade = grade
+
+    def info(self):
+        return f"{super().info()}, Класс: {self.grade}"
+
+student = Student("Ali", 10)
+print(student.info())
+
+
+class Animal():
+    def speak(myself):
+        return "Animals can speak"
+
+class Dog(Animal):
+    def bark(myself):
+        return "Goofy"
+dog= Dog()
+print(dog.speak())
+print(dog.bark())
+
 
