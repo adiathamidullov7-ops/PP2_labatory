ae8d0d394117fe472ba948021cf3e8f858a7f165
adiat <adiathamidullov7@gmail.com>
2026-02-27 00:19:50 +0500

    Json

--- practice4/json.py
+++ practice4/json.py
@@ -1,7 +1,41 @@
-def fun():
-    yield 1
-    yield 2
-    yield 3
+import json
 
-for val in fun():
-    print(val)
+x = '{name: "John", "age":30, "city":"New York"}'
+y = json.load(x)
+print(y["age"])
+
+
+
+import json
+
+x = {
+    "name": "John",
+    "age": "New York"
+}
+y = json.dumps(x)
+print(y)
+
+import json
+
+print(json.dumps({"banana" , "apple", "pinapple"}))
+print(json.dumps("age: 30"))
+print(json.dumps("hello"))
+
+
+import json
+
+x = {
+    "name": "John",
+    "age": 30,
+    "married": True,
+    "divorced": False,
+    "children": ("Ann", "Billy"),
+    "pets": None,
+    "cars": [
+        {"model": "BMW M5 F90", "mpg": 27.5}
+        {"model": "Ford Mustang Shelby", "mpg": 24.1}
+
+    ]
+
+}
+print(json.dumps(x))
