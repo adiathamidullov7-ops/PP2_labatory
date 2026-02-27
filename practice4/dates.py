3b5fc8c8aedb6eb0197b6f4c0ffdebdb68bfa29a
adiat <adiathamidullov7@gmail.com>
2026-02-27 13:18:53 +0500

    dates

--- practice4/dates.py
+++ practice4/dates.py
@@ -1,32 +1,48 @@
-mytyple = ("apple", "banana", "cherry")
-my = iter(mytyple)
-
-print(next(my))
-print(next(my))
-print(next(my))
+from datetime import datetime, timedelta
 
 
-
-mystr = "banana"
-for x in mystr:
-    print(x)
+current_date = datetime.now()
 
 
-class Numbers:
-    def __iter__(self):
-        self.a = 1
-        return self
-    
-    def __next__(self):
-        x = self.a
-        self.a += 1
-        return x
-    
-myClass = Numbers()
-myiter = iter(myClass)
+new_date = current_date - timedelta(days=5)
 
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
-print(next(myiter))
+print("Current date:", current_date.strftime("%Y-%m-%d"))
+print("Date after subtracting 5 days:", new_date.strftime("%Y-%m-%d"))
+
+
+
+from datetime import datetime, timedelta
+today = datetime.now().date()
+yesteray = today - timedelta(days=1)
+tomorrow = today + timedelta(days=1)
+
+print("Yesterday", yesteray)
+print("Today", today)
+print("Tomorrow", tomorrow)
+
+from datetime import datetime
+now = datetime.now()
+print("Datetime now", now)
+
+no_microsecond = now.replace(microsecond=0)
+print("Without microsecond", no_microsecond)
+
+
+
+
+
+
+from datetime import datetime
+
+
+date1_str = input("Enter the first date (YYYY-MM-DD HH:MM:SS): ")
+date2_str = input("Enter the second date (YYYY-MM-DD HH:MM:SS): ")
+
+date1 = datetime.strptime(date1_str, "%Y-%m-%d %H:%M:%S")
+date2 = datetime.strptime(date2_str, "%Y-%m-%d %H:%M:%S")
+
+
+delta = date2 - date1
+
+
+print("Difference in seconds:", int(delta.total_seconds()))
