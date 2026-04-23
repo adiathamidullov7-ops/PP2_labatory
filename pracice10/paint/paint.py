2453a4deef7c367e1c83c6c0436961f28b3c6121
adiat <adiathamidullov7@gmail.com>
2026-04-24 03:07:11 +0500

    Paaiint

--- pracice10/paint/paint.py
+++ pracice10/paint/paint.py
@@ -8,7 +8,7 @@ pygame.display.set_caption("Paint App V2")
 
 clock = pygame.time.Clock()
 
-# Цвета
+
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (220, 220, 220)
@@ -17,11 +17,11 @@ GREEN = (50, 200, 50)
 BLUE = (50, 50, 220)
 YELLOW = (240, 240, 50)
 
-# Холст
+
 canvas = pygame.Surface((WIDTH, HEIGHT))
 canvas.fill(WHITE)
 
-# Настройки
+
 color = BLACK
 brush_size = 6
 eraser = False
@@ -32,17 +32,17 @@ font = pygame.font.SysFont(None, 24)
 def draw_toolbar():
     pygame.draw.rect(screen, GRAY, (0, 0, WIDTH, 60))
 
-    # кнопки цветов
+   
     colors = [BLACK, RED, GREEN, BLUE, YELLOW]
     for i, c in enumerate(colors):
         pygame.draw.rect(screen, c, (10 + i * 50, 10, 40, 40))
 
-    # ластик
+   
     pygame.draw.rect(screen, WHITE, (300, 10, 80, 40))
     text = font.render("ERASER", True, BLACK)
     screen.blit(text, (305, 20))
 
-    # очистка
+    
     pygame.draw.rect(screen, (150, 150, 150), (400, 10, 80, 40))
     text2 = font.render("CLEAR", True, BLACK)
     screen.blit(text2, (410, 20))
@@ -56,12 +56,12 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        # нажатие мыши
+      
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = event.pos
 
             if y < 60:
-                # выбор цвета
+               
                 if 10 < x < 50:
                     color = BLACK
                     eraser = False
@@ -92,13 +92,13 @@ while running:
         if event.type == pygame.MOUSEBUTTONUP:
             drawing = False
 
-    # рисование
+   
     if drawing:
         mx, my = pygame.mouse.get_pos()
         draw_color = WHITE if eraser else color
         pygame.draw.circle(canvas, draw_color, (mx, my), brush_size)
 
-    # вывод
+  
     screen.blit(canvas, (0, 0))
     draw_toolbar()
 
